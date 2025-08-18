7fa20a5449a085538ea3f4038fd69f521a3c9517
SohyeonKim-dev <happythgus89@gmail.com>
2025-08-18 22:17:43 +0900

    [FEAT] 2-1 Solve

--- 02_BruteForce/2-1.cpp
+++ 02_BruteForce/2-1.cpp
@@ -5,12 +5,34 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+// 더 큰 수가 앞에 위치하도록 
+void swaps (vector<int>& input) { // reference 추가 
+    if (input[0] < input[1]){
+        swap(input[0], input[1]);
+    }
+}
 
 int solution(vector<vector<int>> sizes) {
     int answer = 0;
     
+    // 우선 전체에 swap 적용 
+    for (int i = 0; i < sizes.size(); i++) {
+        swaps(sizes[i]);
+    }
     
-    return answer;
+    int max_first = (*max_element(sizes.begin(), sizes.end(),
+                                  [](const vector<int>& a, const vector<int>& b) {
+                                      return a[0] < b[0];  
+                                  }))[0];
+    
+    int max_second = (*max_element(sizes.begin(), sizes.end(),
+                                  [](const vector<int>& a, const vector<int>& b) {
+                                      return a[1] < b[1];  
+                                  }))[1];
+
+    return max_first * max_second;
 }
