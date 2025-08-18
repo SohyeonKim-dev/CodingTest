84c210f08a54fed214170e1294e88a2730cae463
SohyeonKim-dev <happythgus89@gmail.com>
2025-08-18 21:51:19 +0900

    [FEAT] 2-1 Solving

---
+++ 02_BruteForce/2-1.cpp
@@ -0,0 +1,16 @@
+// 최소 직사각형 
+// https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+// 모든 명함을 수납할 수 있는 가장 작은 지갑
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    
+    
+    return answer;
+}
