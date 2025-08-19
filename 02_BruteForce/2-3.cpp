104d898c443751c3492c69647d77130076658b88
SohyeonKim-dev <happythgus89@gmail.com>
2025-08-19 11:50:00 +0900

    [FEAT] 2-3 Solving

---
+++ 02_BruteForce/2-3.cpp
@@ -0,0 +1,25 @@
+// 소수 찾기
+// https://school.programmers.co.kr/learn/courses/30/lessons/42839
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool is_prime(int num) {
+    if (num == 1) return true;
+    
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    
+    for (int i = 0; )
+    
+    
+    return answer;
+}
