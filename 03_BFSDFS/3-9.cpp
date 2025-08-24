26b4513e4f713aea8c382f1af236e505cf495635
SohyeonKim-dev <happythgus89@gmail.com>
2025-08-24 16:18:15 +0900

    [FEAT] 3-9 Solve

--- 03_BFSDFS/3-9.cpp
+++ 03_BFSDFS/3-9.cpp
@@ -9,9 +9,11 @@
 using namespace std;
 
 int t;
+vector<int> answer;
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+// 나이트의 이동 
+int dx[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int dy[8] = {1, -1, 2, -2, 2, -2, 1, -1};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -20,20 +22,48 @@ int main() {
     cin >> t;
 
     for (int tc = 0; tc < t; tc++) {
-        int move;
+        int n;
         int posx, posy;
         int desx, desy;
         
         int board[300][300];
-        bool visited[300][300];
 
-        cin >> move;
+        memset(board, -1, sizeof(board));
+
+        cin >> n;
         cin >> posx >> posy;
         cin >> desx >> desy;
 
+        // posx, posy에서 출발 
+        queue<pair<int, int>> Q;
+        board[posx][posy] = 0;
 
+        Q.push({posx, posy});
 
+        while(!Q.empty()) {
+            pair<int, int> cur = Q.front();
+            Q.pop();
+
+            if (cur.X == desx && cur.Y == desy) {
+                answer.push_back(board[cur.X][cur.Y]);
+                break;
+            }
+
+            for (int dir = 0; dir < 8; dir++) {
+                int tempx = cur.X + dx[dir];
+                int tempy = cur.Y + dy[dir];
+
+                if (tempx < 0 || tempx >= n || tempy < 0 || tempy >= n) continue;
+                if (board[tempx][tempy] != -1) continue; // 이미 방문 -> pass
+
+                board[tempx][tempy] = board[cur.X][cur.Y] + 1;
+                Q.push({tempx, tempy});
+            }
+        }
     }
-    
+    for (size_t i = 0; i < answer.size(); i++) {
+        cout << answer[i] << '\n' ;
+    }
+
     return 0;
 }
