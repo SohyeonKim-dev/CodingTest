c139415d8c32e257767c45e9e9346915861a17a3
SohyeonKim-dev <happythgus89@gmail.com>
2025-08-26 23:59:16 +0900

    [FEAT] 7-6 Solving

--- 07_DataStructure/7-06.cpp
+++ 07_DataStructure/7-06.cpp
@@ -1,3 +1,21 @@
 // 주식가격
 // https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    int n = prices.size();
+    vector<int> answer;
+    
+    // cur보다 작은 값이, 지금 기준으로 언제 오는지 count  
+    for(int i = 0; i < n; i++) {
+        
+    }
+    
+    
+    return answer;
+}
